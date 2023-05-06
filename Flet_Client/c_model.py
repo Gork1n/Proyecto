c89f07d24d6d18e121cac20cf6ce34fe8630e239
Mateo Gorkin <104701717+Gork1n@users.noreply.github.com>
2023-05-06 05:42:27 -0300

    Add files via upload

---
+++ Flet_Client/c_model.py
@@ -0,0 +1,112 @@
+from flet import *
+import socket
+import pickle
+
+HOST = "127.0.0.1" # Servidor
+PORT = 55555
+HEADER = 10
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+class Abmc:
+    def server():
+        server.connect((HOST,PORT))
+
+    def addnewdata(self,e):
+        list=[]
+        for i in range(0,4):
+            list.append(self.table_list[i].value)
+        data_serial = pickle.dumps(list)
+        data_len = str(len(data_serial))
+        message = bytes(f"{data_len:<{HEADER}}",'utf-8')+ data_serial
+        server.send(message)
+        for i in self.table_list:
+            i.value = ''
+        self.addButton.visible = True
+        self.delButton.visible = False
+        self.edButton.visible = False
+    
+    def editindex(e,n,u,p,pv,self):
+        self.id = int(e)
+        print(self.id)
+        self.table_list[0].value = n
+        self.table_list[1].value = u
+        self.table_list[2].value = p
+        self.table_list[3].value = pv
+        self.youid.value = int(e)
+        self.addButton.visible = False
+        self.delButton.visible = True
+        self.edButton.visible = True
+        self.page.update()
+
+    def removeindex(self,e):
+        try:
+            valor = self.id
+            list = [valor, "_baja"]
+            data_serial = pickle.dumps(list)
+            data_len = str(len(data_serial))
+            message = bytes(f"{data_len:<{HEADER}}",'utf-8')+ data_serial
+            server.send(message)
+            self.page.snack_bar = SnackBar(Text(f'succes delete you id = {self.id}', color='white'),bgcolor='red',)
+            self.page.snack_bar.open = True
+            for i in self.table_list:
+                i.value = ''
+        except:pass
+    
+    def editandsave(self,e):
+        list=[self.id, "_modi"]
+        for i in range(0,4):
+            list.append(self.table_list[i].value)
+        data_serial = pickle.dumps(list)
+        data_len = str(len(data_serial))
+        message = bytes(f"{data_len:<{HEADER}}",'utf-8')+ data_serial
+        server.send(message)
+    
+    def message(n):
+        list = n
+        data_serial = pickle.dumps(list)
+        data_len = str(len(data_serial))
+        message = bytes(f"{data_len:<{HEADER}}",'utf-8')+ data_serial
+        server.send(message)
+    
+    def actualizar_info(self, m):
+        if m == 0:
+            prueba=0
+            data_serial = pickle.dumps(prueba)
+            data_len = str(len(data_serial))
+            message = bytes(f"{data_len:<{HEADER}}",'utf-8')+ data_serial
+            server.send(message)
+
+        while True:
+            try:del self.mytable.rows[0]
+            except:break
+        
+        while True:
+            data_len = server.recv(HEADER)
+            if not data_len:
+                break
+            else:
+                data = b''
+                data += server.recv(int(data_len))
+                data_deserial = pickle.loads(data)
+                fin = data_deserial
+                print(fin)
+                if data_deserial == 0:
+                    break
+                else:
+                    self.mytable.rows.append(
+                        DataRow(
+                            cells=[
+                            DataCell(Text(fin[0])),
+                            DataCell(Text(fin[1])),
+                            DataCell(Text(fin[2])),
+                            DataCell(Text(fin[3])),
+                            DataCell(Text(fin[4]))],
+                        on_select_changed=lambda e:Abmc.editindex(
+                            e.control.cells[0].content.value,
+                            e.control.cells[1].content.value,
+                            e.control.cells[2].content.value,
+                            e.control.cells[3].content.value,
+                            e.control.cells[4].content.value,
+                            self)))
+                    self.page.update()
+                    
---
+++ Flet_Client/c_view.py
@@ -0,0 +1,79 @@
+import flet
+from flet import *
+from c_model import Abmc
+import threading
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = "127.0.0.1" # Servidor
+PORT = 55555      # puerto de envio
+HEADER = 10
+
+class Ventana:
+    def __init__(self,page:Page):
+        self.page = page
+        self.page.scroll = 'always'
+        self.nm = TextField(label='Producto',width=400)
+        self.uni = TextField(label='Unidades',width=400)
+        self.prc = TextField(label='Precio',width=400)
+        self.prc_v = TextField(label='Precio de Venta',width=400)
+        self.youid = TextField(label='')
+        self.table_list = [self.nm, self.uni, self.prc, self.prc_v]
+
+        self.mytable = DataTable(
+            columns=[
+                DataColumn(Text('id')),
+                DataColumn(Text('Producto')),
+                DataColumn(Text('Unidades')),
+                DataColumn(Text('Precio')),
+                DataColumn(Text('Precio de Venta'))],
+            rows=[])
+        self.addButton = ElevatedButton('add new', bgcolor='blue',color='white', on_click=self.addnewdata)
+        self.delButton = ElevatedButton('delete this', bgcolor='red',color='white',on_click=self.removeindex)
+        self.edButton = ElevatedButton('update data', bgcolor='orange',color='white',on_click=self.editandsave)
+        self.delButton.visible = False
+        self.edButton.visible = False
+
+        thread1 = threading.Thread(target=self.aviso_ac, args=())
+        thread1.start()
+        self.page.add(
+            Column([
+                Text('Orgstock',size=30, weight='bold'),
+                self.nm, self.uni, self.prc, self.prc_v,
+                Row([self.addButton,self.edButton,self.delButton]),
+                self.mytable,
+            ]))
+    
+    def addnewdata(self,e):
+        print(self)
+        Abmc.addnewdata(self,e)
+        self.page.update()
+        
+    def editandsave(self,e):
+        Abmc.editandsave(self,e)
+        self.delButton.visible = False
+        self.edButton.visible = False
+        self.addButton.visible = True
+        self.page.update()
+    
+    def removeindex(self,e):
+        Abmc.removeindex(self,e)
+        self.delButton.visible = False
+        self.edButton.visible = False
+        self.addButton.visible = True
+        self.page.update()
+    
+    def server():
+        Abmc.server()
+    
+    def mensaje():
+        Abmc.message(["", "_quit"])
+    
+    def aviso_ac(self):
+        server.connect((HOST, 55555))
+        Abmc.message(1)
+        while True:
+            print(1)
+            data = server.recv(1000).decode('utf-8')
+            if data == "_ac":
+                Abmc.actualizar_info(self, 0)
